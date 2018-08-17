004267ca308c3921e68d42a5639fd485cdf106f4
przemek <wosin7@gmail.com>
2018-08-17 22:35:40 +0200

    quotes

---
+++ script.js
@@ -0,0 +1,33 @@
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+    fetch(quoteUrl, { cache: "no-store" })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    var quoteText = dataElement.innerText.trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    if (tweetText.length > 140) {
+      getQuote();
+  } else {
+      var tweet = tweetLink + encodeURIComponent(tweetText);
+      document.querySelector('.quote').innerText = quoteText;
+      document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+      document.querySelector('.tweet').setAttribute('href', tweet);
+  }
+}
+
